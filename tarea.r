73ba8a19c4e9af2381f86c80d340e9afec7cfa5b
zzmckdl23 <probestus@thrma.com>
2016-04-12 20:57:26 -0500

    means

--- tarea.r
+++ tarea.r
@@ -1,6 +1,17 @@
 
-data <- read.table("NuAstro_4yr_IceCube_Data.txt")
+data <- read.table("NuAstro_4yr_IceCube_Data.txt", header=TRUE)
 
 good <- complete.cases(data)
 
+#data sin los NA
 dataclean <- data[good, ]
+
+#para usar las variables de frente
+attach(dataclean)
+
+#calculo de promedios para distintas topologias
+x<-mean(Med_Ang_Res_deg[Topology=="Shower"])
+y<-mean(Med_Ang_Res_deg[Topology=="Track"])
+
+
+
