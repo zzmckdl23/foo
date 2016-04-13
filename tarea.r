c4cfd9ea72cb0da7505a2dac2faa6e65115319ed
zzmckdl23 <probestus@thrma.com>
2016-04-13 14:29:39 -0500

    funcion update y aplicacion

--- tarea.r
+++ tarea.r
@@ -27,12 +27,32 @@ f <- function(x){as.POSIXct(x*24*60*60,origin='1858-11-17',tz='GMT')}
 #agregar columna a dataclean
 dataclean[,"Time_GMT"] <- c(f(Time_MJD))
 
-#function que bota eventos para un mes en distintos años, deberia entrar 'datanew' y un mes 'x'
+#function que bota eventos para un mes en distintos años, deberia entrar 'datanew' y un mes 'x' entre 0 y 11
 evt_month <- function(data,x){
 p <- as.POSIXlt(subset(data, as.POSIXlt(data$Time_GMT)$mon==x)$Time_GMT)$year
-q <- table(p+1900)
-print(q)
+return(p)
 }
+
+#ejemplo para visualizar la funcion
+print(table(evt_month(dataclean,1)+1900))
+
+
+#encontrar mes con mayor eventos para un año
+
+#seleccionar un año, por ejemplo 113 que equivale a 2013, que tiene 2 meses con maximo numero de eventos (2 eventos)
+#datapr son los datos para el año escogido
+datapr <- dataclean[as.POSIXlt(dataclean$Time_GMT)$year==113, ]
+
+#conseguir numero de eventos para todos los meses
+for(i in unique(as.POSIXlt(datapr$Time_GMT)$mon)){
+  eventos<- length(evt_month(datapr,i)) #aca se usa la funcion
+  qq<-cbind(mes=i+1,eventos)   #i+1 para que el numero de mes corresponda con la convencion de 1:12 en vez de 0:11
+  if(i==min(unique(as.POSIXlt(datapr$Time_GMT)$mon))){qp<-qq}else{qp<-rbind(qp,qq)}
+  #print(qp)
+}
+#se tiene la tabla qp con numero de mes y numero de evento
+#luego se imprime las filas con mayor numero de eventos
+print(qp[qp[,2]==max(qp[,2]), ],header=TRUE)
 
 
 
